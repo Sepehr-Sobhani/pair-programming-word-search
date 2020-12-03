4e6df082994d1177e865cbaa8cd3ee50391fd0c9
Sepehr-Sobhani <sobhani.sepehr@gmail.com>
2020-12-03 03:44:08 +0000

    creating extra functions and tests to improve the buggy code and pass all tests

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,21 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  if (letters.length !== 0 || letters[0].length !== 0) {
+    const horizontalJoin = letters.map((ls) => ls.join(""));
+    const verticalJoin = letters[0].map((_, colIndex) =>
+      letters.map((row) => row[colIndex])
+    );
+    const newVerticalJoin = [];
+    for (let letter of verticalJoin) {
+      newVerticalJoin.push(letter.join(""));
     }
-}
+    for (l of horizontalJoin) {
+      if (l.includes(word)) return true;
+    }
+    for (l of newVerticalJoin) {
+      if (l.includes(word)) return true;
+    }
+    return false;
+  } else return false;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
